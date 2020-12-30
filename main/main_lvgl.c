3cf3b2c665df9154fc57b3cfbe7203df0cb4f5b7
Vedant Paranjape <22630228+VedantParanjape@users.noreply.github.com>
2020-12-30 18:38:14 +0530

    Added example code and updated cmake files

---
+++ main/main_lvgl.c
@@ -0,0 +1,156 @@
+#include <driver/gpio.h>
+#include <driver/spi_master.h>
+#include <esp_log.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include <stdio.h>
+#include <string.h>
+#include <ds3231.h>
+
+#include "oa_power.h"
+#include "oa_pin_defs.h"
+#include "oa_battery.h"
+#include "oa_switches.h"
+
+#include "sdkconfig.h"
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_freertos_hooks.h"
+#include "freertos/semphr.h"
+#include "esp_system.h"
+#include "driver/gpio.h"
+
+/* Littlevgl specific */
+#include "lvgl/lvgl.h"
+#include "lvgl_helpers.h"
+
+/*********************
+ *      DEFINES
+ *********************/
+#define TAG "demo"
+#define LV_TICK_PERIOD_MS 1
+
+/**********************
+ *  STATIC PROTOTYPES
+ **********************/
+static void lv_tick_task(void *arg);
+static void guiTask(void *pvParameter);
+
+
+/**********************
+ *   APPLICATION MAIN
+ **********************/
+void app_main() {
+    
+    init_load_switch();
+    init_switches();
+    activate_load_switch();
+
+    config_adc1();
+    characterize_adc1();
+
+    /* If you want to use a task to create the graphic, you NEED to create a Pinned task
+     * Otherwise there can be problem such as memory corruption and so on.
+     * NOTE: When not using Wi-Fi nor Bluetooth you can pin the guiTask to core 0 */
+    xTaskCreatePinnedToCore(guiTask, "gui", 4096*2, NULL, 0, NULL, 1);
+}
+
+/* Creates a semaphore to handle concurrent call to lvgl stuff
+ * If you wish to call *any* lvgl function from other threads/tasks
+ * you should lock on the very same semaphore! */
+SemaphoreHandle_t xGuiSemaphore;
+
+static void guiTask(void *pvParameter) {
+    (void) pvParameter;
+    xGuiSemaphore = xSemaphoreCreateMutex();
+
+    lv_init();
+    
+    /* Initialize SPI or I2C bus used by the drivers */
+    lvgl_driver_init();
+
+    static lv_color_t buf1[DISP_BUF_SIZE];
+
+    static lv_color_t *buf2 = NULL;
+
+    static lv_disp_buf_t disp_buf;
+
+    uint32_t size_in_px = DISP_BUF_SIZE;
+
+    /* Initialize the working buffer depending on the selected display.
+     * NOTE: buf2 == NULL when using monochrome displays. */
+    lv_disp_buf_init(&disp_buf, buf1, buf2, size_in_px);
+
+    lv_disp_drv_t disp_drv;
+    lv_disp_drv_init(&disp_drv);
+    disp_drv.flush_cb = disp_driver_flush;
+
+    /* When using a monochrome display we need to register the callbacks:
+     * - rounder_cb
+     * - set_px_cb */
+    disp_drv.rounder_cb = disp_driver_rounder;
+    disp_drv.set_px_cb = disp_driver_set_px;
+
+    disp_drv.buffer = &disp_buf;
+    lv_disp_drv_register(&disp_drv);
+    
+    /* Create and start a periodic timer interrupt to call lv_tick_inc */
+    const esp_timer_create_args_t periodic_timer_args = {
+        .callback = &lv_tick_task,
+        .name = "periodic_gui"
+    };
+    esp_timer_handle_t periodic_timer;
+    ESP_ERROR_CHECK(esp_timer_create(&periodic_timer_args, &periodic_timer));
+    ESP_ERROR_CHECK(esp_timer_start_periodic(periodic_timer, LV_TICK_PERIOD_MS * 1000));
+    
+    /* use a pretty small demo for monochrome displays */
+    /* Get the current screen  */
+    lv_obj_t * scr = lv_disp_get_scr_act(NULL);
+
+    /*Create a Label on the currently active screen*/
+    lv_obj_t * label1 =  lv_label_create(scr, NULL);
+    lv_obj_t * label2 = lv_label_create(scr, NULL);
+
+    struct tm time;
+
+    ESP_ERROR_CHECK(i2cdev_init());
+    i2c_dev_t dev;
+    memset(&dev, 0, sizeof(i2c_dev_t));
+
+    ESP_ERROR_CHECK(ds3231_init_desc(&dev, 1, RTC_SDA, RTC_SCL));
+
+    while (1) 
+    {
+        char time_[50], voltage[50];
+        ds3231_get_time(&dev, &time);
+
+        // snprintf(time_, 50, "%d    %d    %d", read_switch(SWITCH_UP), read_switch(SWITCH_DOWN), read_switch(SWITCH_SELECT));
+        snprintf(time_, 50, "%02d:%02d:%02d", time.tm_hour, time.tm_min, time.tm_sec);
+        float volt = (float)battery_percentage()/1000;
+        snprintf(voltage, 50, "%.4f V", volt);
+
+        lv_label_set_text(label1, time_);
+        lv_label_set_text(label2, voltage);
+
+        lv_obj_align(label1, NULL, LV_ALIGN_IN_TOP_MID, 0, 0);
+        lv_obj_align(label2, NULL, LV_ALIGN_IN_BOTTOM_MID, 0, 0);
+        vTaskDelay(100 / portTICK_PERIOD_MS);
+    }
+
+    vTaskDelete(NULL);
+}
+
+static void lv_tick_task(void *arg) {
+    (void) arg;
+    if (pdTRUE == xSemaphoreTake(xGuiSemaphore, portMAX_DELAY)) {
+            lv_task_handler();
+            xSemaphoreGive(xGuiSemaphore);
+       }    
+    lv_tick_inc(LV_TICK_PERIOD_MS);
+}
